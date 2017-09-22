eec7d92eb7ea2b0b915e9bae83c3dd2a40b549a4
DL052148 <david.lewis@mayerbrown.com>
2017-09-22 17:11:08 +0100

    added group policy functions

--- merakiBot/merakiBot.py
+++ merakiBot/merakiBot.py
@@ -44,11 +44,14 @@ def getNetworkIDs(baseURL, headers, orgID):
 def getDevices(baseURL, headers, networkID):
     print "Getting devices..."
     print "number of networks:" + str(len(networkID))
+    s = requests.Session()
+    s.headers.update(headers)
+    s.verify = verifySSL
     dlist = []
     for net in networkID:
         netID = net['id']
         url = baseURL + "/networks/" + str(netID) + "/devices"
-        response = requests.get(url=url, headers=headers, verify=False)
+        response = s.get(url=url)
         if response.status_code == 200:
             deviceIDs = json.loads(response.text)
 
@@ -59,6 +62,7 @@ def getDevices(baseURL, headers, networkID):
                 dlist.append(emptydev)
         else:
             print "There was an issue with your request. Here is the error text:\n" + response.text
+    s.close()
     return dlist
 
 
@@ -74,16 +78,58 @@ def getClientDetails2(baseURL, headers, deviceID, timespan="86400"):
         response = s.get(url=url)
         if response.status_code == 200:
             clients = json.loads(response.text)
+            i=0
             for client in clients:
+                print "client number: " + i
+                i=i+1
                 emptycli = {}
                 emptycli['description'] = client['description']
                 emptycli['ip'] = client['ip']
                 emptycli['mac'] = client['mac']
+                emptycli['apID'] = client['id']
+                emptycli['apName'] = (item for item in devices if item["serial"] == client["id"])
                 clist.append(emptycli)
         else:
             print "There was an issue with your request. HTTP Code was:" + response.status_code + ". Here is the error text:\n" + response.text
     dedupe = [i for n, i in enumerate(clist) if i not in clist[n + 1:]]
+    s.close()
     return dedupe
+
+
+def getGroupPolicies(baseURL, headers, networkID):
+    print "Getting policies..."
+    s = requests.Session()
+    s.headers.update(headers)
+    s.verify = verifySSL
+    plist = []
+    url = baseURL + "/networks/" + str(networkID) + "/groupPolicies"
+    response = s.get(url=url)
+    if response.status_code == 200:
+        policies = json.loads(response.text)
+        for policy in policies:
+            emptydev = {}
+            emptydev['name'] = policy['name']
+            emptydev['groupPolicyId'] = policy['groupPolicyId']
+            plist.append(emptydev)
+    else:
+         print "There was an issue with your request. Here is the error text:\n" + response.text
+    s.close()
+    return plist
+
+
+def getClientPolicy(baseURL, headers, networkID, clientMAC, timespan = "86400"):
+    s = requests.Session()
+    s.headers.update(headers)
+    s.verify = verifySSL
+    url = baseURL + "/networks/" + str(networkID) + "/clients/" + clientMAC + "/policy?timespan=" + timespan
+    response = s.get(url=url)
+    if response.status_code == 200:
+        policy = json.loads(response.text)
+        policyID = policy["groupPolicyId"]
+    else:
+        print "There was an issue with your request. Here is the error text:\n" + response.text
+    s.close()
+    return json.dumps(policyID)
 
 
 if __name__ == "__main__":
@@ -93,7 +139,9 @@ if __name__ == "__main__":
     organisationID = organisations[0]['id']
     print "organisationID = " + str(organisationID)
     networkIDs = getNetworkIDs(baseURL, headers, organisationID)
-    print len(networkIDs)
     devices = getDevices(baseURL, headers, networkIDs)
     clients = getClientDetails2(baseURL, headers, devices)
+    client = next((item for item in clients if item["ip"] == "172.30.22.41"), "Not found")
+    groupPolicies = getGroupPolicies(baseURL, headers, networkIDs)
+
 
